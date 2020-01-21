e9e32c6e9e012bad018804461a7b869a19e7cd68
doseeing <doseeing@gmail.com>
2020-01-21 17:24:40 +0800

    remove unused lib import

--- template/src/renderer/main.js
+++ template/src/renderer/main.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-{{#isEnabled plugins 'axios'}}
-import axios from 'axios'
-{{/isEnabled}}
-
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
